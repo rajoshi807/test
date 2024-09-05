46ad46f2a63ba209e73e420b3d7b89b88c4e8c07
Ruhi Joshi <rajoshi807@gmail.com>
2024-09-05 10:13:15 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    vector <int> arr = {1, 2, 3, 4};
+    unsigned seed = 0;
+ 
+    cout << "Original array:";
+ 
+    for (int ele: arr)
+    {
+        cout << ele << " ";
+    }
+ 
+    cout << endl;
+ 
+    shuffle(arr.begin(), arr.end(), default_random_engine(seed));
+ 
+    cout << "Shuffled array:";
+ 
+    for (int ele: arr)
+    {
+        cout << ele << " ";
+    }
+ 
+    return 0;
+}
