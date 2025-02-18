f32cc06455c56b7e31b79e1635a1d790c1332b45
Ruhi Joshi <rajoshi807@gmail.com>
2025-02-18 11:31:13 +0530

    Create try.h

---
+++ try.h
@@ -0,0 +1,15 @@
+#include<stdio.h> 
+
+static hello<IAbcd> current_context()
+{
+  return context;
+}
+
+int main() 
+{ 
+    char a = 30; 
+    char b = 40; 
+    char c = 10; 
+    char d = (a * b) / c;   
+    return 0; 
+} 
