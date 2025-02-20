32e7db289c6cdd28df25340d326bcc3da231d32b
Ruhi Joshi <rajoshi807@gmail.com>
2025-02-20 14:23:59 +0530

    Update try.vb

--- try.vb
+++ try.vb
@@ -8,3 +8,24 @@ Module Program
         Console.ReadKey(True)
     End Sub
 End Module
+
+Imports System.Xml
+
+Public Class XMLExample
+    Public Sub ParseXML()
+        ' XML content with angle brackets
+        Dim xmlString As String = "<person><name>John Doe</name><age>30</age></person>"
+
+        ' Load XML into an XmlDocument
+        Dim xmlDoc As New XmlDocument()
+        xmlDoc.LoadXml(xmlString)
+
+        ' Access the elements in XML
+        Dim name As String = xmlDoc.SelectSingleNode("//name").InnerText
+        Dim age As String = xmlDoc.SelectSingleNode("//age").InnerText
+
+        ' Display the extracted values
+        Console.WriteLine("Name: " & name)
+        Console.WriteLine("Age: " & age)
+    End Sub
+End Class
