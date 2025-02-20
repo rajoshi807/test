eda11f2bed9b94b9a34b996f8789d23818836cf5
Ruhi Joshi <rajoshi807@gmail.com>
2025-02-20 14:08:18 +0530

    Create try.jsx

---
+++ try.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Greeting() {
+  const name = "Alice";
+  return (
+    <div>
+      <h1>Hello, {name}!</h1>
+      <p>Welcome to JSX!</p>
+    </div>
+  );
+}
+
+export default Greeting;
