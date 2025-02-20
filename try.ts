17986e8ca5a2aab83a5fd8a44b8bac33dcd961a5
Ruhi Joshi <rajoshi807@gmail.com>
2025-02-20 13:53:58 +0530

    Update try.ts

--- try.ts
+++ try.ts
@@ -3,6 +3,17 @@ function identity<T>(value: T): T {
     return value;
 }
 
+function getAppropriateReplyToId(source: Partial<Activity>): string | undefined {
+    if (
+        source.type !== ActivityTypes.ConversationUpdate ||
+        (source.channelId !== Channels.Directline && source.channelId !== Channels.Webchat)
+    ) {
+        return source.id;
+    }
+
+    return undefined;
+}
+
 // Using the generic function with different types
 const numberValue = identity<number>(42);
 const stringValue = identity<string>("Hello, TypeScript");
