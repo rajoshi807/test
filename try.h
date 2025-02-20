370477f48b6fef62fe7a4c7202c99b794db4fe00
Ruhi Joshi <rajoshi807@gmail.com>
2025-02-20 14:35:17 +0530

    Update try.h

--- try.h
+++ try.h
@@ -17,6 +17,34 @@ static hello<IAbcd> current_context()
         return context;
     }
 
+#include <iostream>
+#include <algorithm>
+
+/**
+ * Literal class type that wraps a constant expression string.
+ *
+ * Uses implicit conversion to allow templates to *seemingly* accept constant strings.
+ */
+template<size_t N>
+struct StringLiteral {
+    constexpr StringLiteral(const char (&str)[N]) {
+        std::copy_n(str, N, value);
+    }
+    
+    char value[N];
+};
+
+template<StringLiteral lit>
+void Print() {
+    // The size of the string is available as a constant expression.
+    constexpr auto size = sizeof(lit.value);
+
+    // and so is the string's content.
+    constexpr auto contents = lit.value;
+
+    std::cout << "Size: " << size << ", Contents: " << contents << std::endl;
+}
+
 int main() 
 { 
     char a = 30; 
@@ -24,5 +52,6 @@ int main()
     char c = 10; 
     char d = (a * b) / c;   
     vector<int>hello;
+    Print<"literal string">();
     return 0; 
 } 
