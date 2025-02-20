407fa92b731cf259c8619da9cccec2f74aedfa2e
Ruhi Joshi <rajoshi807@gmail.com>
2025-02-20 10:55:17 +0530

    Update try.pl

--- try.pl
+++ try.pl
@@ -5,6 +5,10 @@ use warnings;
 # Open a file for reading
 open(my $fh, '<', 'example.txt') or die "Cannot open file: $!";
 
+<html>
+<div>abre14</div>
+</html>
+
 # Read the file line by line
 while (my $line = <$fh>) {
     print $line;
